ee550e3484a8f7443116103ee4e7602f97e65bd6
Diego Dias <ddias.py@gmail.com>
2024-07-25 22:41:17 -0300

    exmplo de templates

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,"home.html")
+
+    
+
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
