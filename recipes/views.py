fa0cbad79d6527d4c51972be1c605b8f8cbd5515
Diego Dias <ddias.py@gmail.com>
2024-07-27 15:18:03 -0300

    Editando CSS e HTML dividindo em em partes

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={'name':'Diego Dias'})
+    return render(request, "recipes/pages/home.html", context={'name':'Diego Dias'})
 
 
 
