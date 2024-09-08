954a4f824ff84bca9279364c9e23b031dcbf3708
Diego Dias <ddias.py@gmail.com>
2024-09-08 20:06:28 -0300

    queryset

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
