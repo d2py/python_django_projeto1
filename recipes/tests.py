bdb9e3d2421ade203dd18a2e36cf8db1e1030c5f
Diego Dias <ddias.py@gmail.com>
2025-01-08 15:25:27 -0300

    instalando pytest e fazendo teste

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = '123456'
+        print('OLÁ MUNDO')
+        assert 1 == 1, 'um é igaul a um'
