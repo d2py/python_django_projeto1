bac7a465175689357d4ce365b7da3b029039c2d1
Diego Dias <ddias.py@gmail.com>
2025-02-17 16:09:20 -0300

    Criação da funçao 404 teste

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -10,16 +10,10 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home')) 
         self.assertIs(view.func, views.home)
 
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1})) 
-        self.assertIs(view.func, views.category)    
+       
 
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1})) 
-        self.assertIs(view.func, views.recipe)    
-
-    def test_recipe_home_view_returns_status_code_200_ok(self):   
-        response = self.client.get(reverse('recipes:home'))
+    def test_recipe_category_view_returns_404_if_no_recipes(self):   
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_view_loads_correct_template(self):   
@@ -29,3 +23,12 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
          response = self.client.get(reverse('recipes:home'))
          self.assertIn('<h1> No recipes found here </h1>', response.content.decode('utf-8'))
+
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1})) 
+        self.assertIs(view.func, views.category)    
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1})) 
+        self.assertIs(view.func, views.recipe)      
